b8c0fb6aee48dc12b352282d1a8704426e5e2646
konosato-idcf <konosato@idcf.jp>
2019-10-31 18:59:59 +0900

    first commit

---
+++ server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/users/:id", getUser)
+	e.Logger.Fatal(e.Start(":1323"))
+
+	//e.POST("/users", saveUser)
+
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
