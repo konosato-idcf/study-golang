01aa446b03a753ba80bd4dd6ecb363e27606fdb4
konosato-idcf <konosato@idcf.jp>
2019-12-02 13:53:43 +0900

    writing test

--- echo-OJT-webApp/mysql/initdb.d/schema.sql
+++ echo-OJT-webApp/mysql/initdb.d/schema.sql
@@ -1,4 +1,13 @@
-CREATE TABLE users
+CREATE TABLE sample_app.users
+(
+    id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(45)  NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE DATABASE sample_app_test;
+
+CREATE TABLE sample_app_test.users
 (
     id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name  VARCHAR(45)  NOT NULL,
--- echo-OJT-webApp/user/handlers_test.go
+++ echo-OJT-webApp/user/handlers_test.go
@@ -6,9 +6,12 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang/mock/gomock"
 	"github.com/konosato-idcf/study-golang/echo-OJT-webApp/user/infra/models"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	"github.com/stretchr/testify/assert"
+	"github.com/volatiletech/sqlboiler/queries/qm"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -41,6 +44,7 @@ func GetEchoContext(path string, requestMethod string, requestJson string) (echo
 // https://github.com/golang/go/wiki/TableDrivenTests
 // https://qiita.com/yut-kt/items/5f9eb752f40d4d2a2e97
 func TestUsersHandler_Create_Validation(t *testing.T) {
+	// 種別：正常
 	casesOk := []struct {
 		Name  string `json:"name"`
 		Email string `json:"email"`
@@ -48,6 +52,8 @@ func TestUsersHandler_Create_Validation(t *testing.T) {
 		{"k", "k@gmail.com"},
 		{"aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeee",
 			"aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeee@idcf.jp"},
+		{"longEmail",
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@idcf.jp"},
 	}
 	for _, tt := range casesOk {
 		bytes, err := json.Marshal(tt)
@@ -55,86 +61,137 @@ func TestUsersHandler_Create_Validation(t *testing.T) {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(bytes)
 		requestJson := string(bytes)
-		c, rec := GetEchoContext("/user", http.MethodPost, requestJson)
+		c, _ := GetEchoContext("/users", http.MethodPost, requestJson)
 		u := new(User)
-		if assert.NoError(t, c.Bind(u)) {
-			assert.Equal(t, http.StatusOK, rec.Code)
-		}
-		if assert.NoError(t, c.Validate(u)) {
-			assert.Equal(t, http.StatusOK, rec.Code)
-		}
+		assert.NoError(t, c.Bind(u))
+		assert.NoError(t, c.Validate(u))
 	}
+
+	// 種別：異常
+	casesNg := []struct {
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	}{
+		{"aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeee",
+			"tooLongName@idcf.jp"},
+		{"",
+			"emptyName@idcf.jp"},
+		{"notEmailFormat",
+			"aaaidcf.jp"},
+		{"tooLongEmail",
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@idcf.jp"},
+		{"emptyEmail",
+			""},
+	}
+	for _, tt := range casesNg {
+		bytes, err := json.Marshal(tt)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		requestJson := string(bytes)
+		c, _ := GetEchoContext("/users", http.MethodPost, requestJson)
+		u := new(User)
+		assert.NoError(t, c.Bind(u))
+		assert.Error(t, c.Validate(u))
+	}
+
+	// 種別：異常、JSON形式でないリクエスト
+	caseNotJson := `{name:Joe,email:joe@idcf.jp}`
+	c, _ := GetEchoContext("/users", http.MethodPost, caseNotJson)
+	u := new(User)
+	assert.Error(t, c.Bind(u))
+	assert.Error(t, c.Validate(u))
+
+	// 種別：異常、リクエストパラーメータにIDを加えて送る
+	//caseAddId :=  `{"id":10, "name":"Joe","email":"joe@idcf.jp", "hoge":"huga"}`
+	//c, _ = GetEchoContext("/users", http.MethodPost, caseAddId)
+	//u = new(User)
+	//assert.NoError(t, c.Bind(u))
+	//assert.NoError(t, c.Validate(u))
 }
 
 // バリデーションチェックが全て通った場合、ユーザーが登録される。
-//func TestUsersHandler_Create(t *testing.T) {
-//	testDb, err := ConnectDatabase(testConfig)
-//	if err != nil {
-//		panic(err)
-//	}
-//	defer deleteUser(testDb)
-//
-//	requestJson := `{"name":"Joe","email":"joe@idcf.jp"}`
-//	c, rec := GetEchoContext("/users/:id", http.MethodPost, requestJson)
-//
-//	u := NewUser(testDb)
-//	h := NewUsersHandler(u)
-//
-//	// Assertion
-//	// want := `{"id":0,"name":"Joe","email":"joe@idcf.jp"}`
-//	want := &User{
-//		Name:  "Joe",
-//		Email: "joe@idcf.jp",
-//	}
-//
-//	if assert.NoError(t, h.Create(c)) {
-//		assert.Equal(t, http.StatusCreated, rec.Code)
-//
-//		responseUser := &User{}
-//		json.Unmarshal(rec.Body.Bytes(), responseUser)
-//		// fmt.Println(responseUser)
-//		// assert.Equal(t, want, strings.TrimSpace(rec.Body.String()))
-//		assert.GreaterOrEqual(t, responseUser.ID, 0)
-//		assert.Equal(t, want.Name, responseUser.Name)
-//		assert.Equal(t, want.Email, responseUser.Email)
-//
-//		// DBのユーザーテーブルの値の確認
-//		ctx := context.Background()
-//		user, err := models.Users(qm.Where("id=?", responseUser.ID)).One(ctx, testDb)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		assert.Equal(t, want.Name, user.Name)
-//		assert.Equal(t, want.Email, user.Email)
-//	}
-//}
+func TestUsersHandler_Create(t *testing.T) {
+	testDb, err := ConnectDatabase(testConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer deleteUser(testDb)
 
-//func TestUsersHandler_Update(t *testing.T) {
-//	requestJson := `{"name":"Joe","email":"joe2@idcf.jp"}`
-//	c, rec := GetEchoContext("/users/:id", http.MethodPut, requestJson)
-//  c.SetParamNames("id")
-//  c.SetParamValues(1)
-//
-//	ctrl := gomock.NewController(t)
-//	defer ctrl.Finish()
-//
-//	u := NewMockUsersInterface(ctrl)
-//	u.EXPECT().FindById(gomock.Any(), 0).Return(&User{
-//		ID: 0,
-//		Name: "Joe",
-//		Email: "joe@idcf.jp",
-//	}, nil)
-//	h := NewUsersHandler(u)
-//
-//	// Assertion
-//	want := `{"id":0,"name":"Joe","email":"joe2@idcf.jp"}`
-//	if assert.NoError(t, h.Update(c)) {
-//		assert.Equal(t, http.StatusCreated, rec.Code)
-//		assert.Equal(t, want, strings.TrimSpace(rec.Body.String()))
-//	}
-//}
+	// Case: Pass test
+	requestJson := `{"name":"Joe","email":"joe@idcf.jp"}`
+	c, rec := GetEchoContext("/users", http.MethodPost, requestJson)
+
+	u := NewUser(testDb)
+	h := NewUsersHandler(u)
+
+	// Assertion
+	// want := `{"id":1,"name":"Joe","email":"joe@idcf.jp"}`
+	want := &User{
+		Name:  "Joe",
+		Email: "joe@idcf.jp",
+	}
+
+	if assert.NoError(t, h.Create(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		responseUser := &User{}
+		json.Unmarshal(rec.Body.Bytes(), responseUser)
+		// fmt.Println(responseUser)
+		// assert.Equal(t, want, strings.TrimSpace(rec.Body.String()))
+		assert.GreaterOrEqual(t, responseUser.ID, 0)
+		assert.Equal(t, want.Name, responseUser.Name)
+		assert.Equal(t, want.Email, responseUser.Email)
+
+		// DBのユーザーテーブルの値の確認
+		ctx := context.Background()
+		user, err := models.Users(qm.Where("id=?", responseUser.ID)).One(ctx, testDb)
+		if err != nil {
+			log.Fatal(err)
+		}
+		assert.Equal(t, want.Name, user.Name)
+		assert.Equal(t, want.Email, user.Email)
+	}
+
+	// Case: NG test
+	//requestJsonNg := `{"name":"Joe","email":"joe@idcf.jp"}`
+	//cNg, recNg := GetEchoContext("/users", http.MethodGet, requestJsonNg)
+	//fmt.Println(cNg)
+	//
+	//u = NewUser(testDb)
+	//h = NewUsersHandler(u)
+	//
+	//if assert.Error(t, h.Create(cNg)) {
+	//	assert.Equal(t, http.StatusMethodNotAllowed, recNg.Code)
+	//}
+}
+
+func TestUsersHandler_Update(t *testing.T) {
+	requestJson := `{"name":"Joe","email":"joe2@idcf.jp"}`
+	c, rec := GetEchoContext("/users/:id", http.MethodPut, requestJson)
+	c.SetParamNames("id")
+	c.SetParamValues("2")
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := NewMockUsersInterface(ctrl)
+	u.EXPECT().FindById(gomock.Any(), 2).Return(&User{
+		ID:    2,
+		Name:  "Joe",
+		Email: "joe@idcf.jp",
+	}, nil)
+	h := NewUsersHandler(u)
+
+	// Assertion
+	want := `{"id":2,"name":"Joe","email":"joe2@idcf.jp"}`
+	if assert.NoError(t, h.Update(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, want, strings.TrimSpace(rec.Body.String()))
+	}
+}
 
 func TestMain(m *testing.M) {
 	testDb, err := ConnectDatabase(testConfig)
